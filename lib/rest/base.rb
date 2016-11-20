de2ee1f635b71d2788d6f13a15222d5c2db7ba1c
Egor Smolyakov <egorsmkv@gmail.com>
2016-11-20 02:21:56 +0200

    Add base handler

---
+++ lib/rest/base.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+
+module Rest
+  module Whois
+    class Base < Sinatra::Base
+      configure do
+        disable :method_override
+        disable :static
+      end
+
+      before do
+        content_type 'application/json; charset=utf-8'
+      end
+
+      not_found do
+        JSON.pretty_generate(error: 'Resource not found.')
+      end
+    end
+  end
+end
